3c71e3709c5d39d4937fe1b569131f79811349af
Davidek523 <davidov520@wp.pl>
2025-09-15 14:56:31 +0200

    small change
    a small change

--- facts_generator.py
+++ facts_generator.py
@@ -12,7 +12,7 @@ def class_contributions():
         ("Ruud", "Ik ben getrouwd, maar woon niet samen."),
         
         # 👇 Students, add your entries here:
-        ("Davit", "Ik ben een student")
+        ("Davit", "Ik ben een student"),
 
         ("Ahmed", "Hoi"),
 
